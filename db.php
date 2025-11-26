6876e976c879c46cf039994d688ff7a4ebacb836
patledor <ewebledor@gmail.com>
2025-11-26 18:33:57 +0800

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ $host = getenv('DB_HOST');
 $user = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $dbname = getenv('DB_NAME');
-$port = getenv('DB_PORT') ?: 5432;
+$port = getenv('DB_PORT');
 
 // Tiyaking kasama ang sslmode=require para sa Neon
 $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require"; 
@@ -16,3 +16,4 @@ try {
     die("DB ERROR: " . $e->getMessage());
 }
 ?>
+
